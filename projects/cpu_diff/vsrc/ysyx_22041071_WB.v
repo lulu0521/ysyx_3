b03ba52c6b8f2dc13294c3b8c711dd242862fb7b
tracer-oscpu2021 <tracer@oscpu.org>
2022-09-13 20:06:56 +0800

    MJL ysyx_22041071    Static hostname: majianlu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 794a20b421c842dbb5e932ca71cc4fcd            Boot ID: 09ca29278c144d20a2438c9532f9b902     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-46-generic       Architecture: x86-64  20:06:56 up  9:27,  1 user,  load average: 0.12, 0.03, 0.04

--- projects/cpu_diff/vsrc/ysyx_22041071_WB.v
+++ projects/cpu_diff/vsrc/ysyx_22041071_WB.v
@@ -25,11 +25,7 @@ module ysyx_22041071_WB(
 		valid7	  = valid6		;
 		reg_w_en5 = reg_w_en4	;	
 		rdest4	  = rdest3	  	;	 		
-		WB_data2  = WB_data1 	;
-		$display("*******************************PC6 = %x ",PC6);
-		$display("*******************************reg_w_en5 = %x ",reg_w_en5);
-		$display("*******************************WB_data2 = %x ",WB_data1);	
-		$display("******************************* rdest4 = %x",rdest4 );		
+		WB_data2  = WB_data1 	;	
 	end
 	
 endmodule
