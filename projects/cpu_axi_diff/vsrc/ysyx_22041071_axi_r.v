5392b76fb626cfada687dd803a18331bf1293002
tracer-oscpu2021 <tracer@oscpu.org>
2022-10-20 10:31:07 +0800

    MJL ysyx_22041071    Static hostname: majianlu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 794a20b421c842dbb5e932ca71cc4fcd            Boot ID: f52d74f2ba2a4ad4b032e0151073efbf     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  10:31:07 up 58 min,  1 user,  load average: 0.69, 0.31, 0.20

--- projects/cpu_axi_diff/vsrc/ysyx_22041071_axi_r.v
+++ projects/cpu_axi_diff/vsrc/ysyx_22041071_axi_r.v
@@ -75,7 +75,7 @@ module ysyx_22041071_axi_r(
 
 	
 	assign axi_ar_valid_o_	= c_state == READ_ADDR 				;
-	assign axi_r_ready_o_	= c_state == READ_DATA	|| c_state == READ_IDLE;
+	assign axi_r_ready_o_	= c_state == READ_DATA	;//|| c_state == READ_IDLE
 	assign ar_handshake		= axi_ar_valid_o_ & axi_ar_ready_i	;
 	assign r_handshake		= axi_r_valid_i   & axi_r_ready_o_	;
 	assign r_done			= r_handshake	  & axi_r_last_i	;
